6f18322bc4c737f8021018500fdea9459115f0ba
unknown <haoliang@vcredit.cn>
2016-03-15 18:10:19 +0800

    2

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var http = require('http');
 http.createServer(function (request, response) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
     
-    
     GetRsgPwd(response);
  }).listen(8124);
 
