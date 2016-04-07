bd2517571c6d2a821a541c835c70c36e199e00be
leo <hl4974@hotmail.com>
2016-04-07 18:05:15 +0800

    commit

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ function GetRsgPwd(response)
         url: "https://login.taobao.com/member/login.jhtml",
         scripts: ["http://code.jquery.com/jquery.js" // jquery 
                 , "https://g.alicdn.com/kissy/k/1.4.4/seed-min.js" // kissy js
-                , "https://g.alicdn.com/kg/??rsa/2.0.1/index-min.js?t=1358514278.js"], // Ã‘±¶√‹¬Îº”√‹js
+                , "https://g.alicdn.com/kg/??rsa/2.0.1/index-min.js?t=1358514278.js"], // ??????????js
     done: function (err, window) {
         var $ = window.$;
         console.log("dom is done");
--- appV3.js
+++ appV3.js
@@ -15,30 +15,34 @@ app.get('/', function(req, res){
 
 app.listen(3000);
 
-
 function GetRsgPwd(response, oldPwd)
 {
     jsdom.env({
         url: "https://login.taobao.com/member/login.jhtml",
-        scripts: ["http://code.jquery.com/jquery.js" // jquery 
+        scripts: ["http://localhost:10180/scripts/jquery-2.2.2.js" // jquery 
                 , "https://g.alicdn.com/kissy/k/1.4.4/seed-min.js" // kissy js
-                , "https://g.alicdn.com/kg/??rsa/2.0.1/index-min.js?t=1358514278.js"], // ÌÔ±¦ÃÜÂë¼ÓÃÜjs
-    done: function (err, window) {
-        var $ = window.$;
-        console.log("dom is done");
-        
-        window.KISSY.use("kg/rsa/2.0.1/index",function (S,e)
+                , "https://g.alicdn.com/kg/??rsa/2.0.1/index-min.js?t=1358514278.js"], // ï¿½Ô±ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½js
+        done: function (err, window) 
         {
-            var rsa = new e;
-            var pbk = $("#J_PBK").val();
-            // var pbk = "9a39c3fefeadf3d194850ef3a1d707dfa7bec0609a60bfcc7fe4ce2c615908b9599c8911e800aff684f804413324dc6d9f982f437e95ad60327d221a00a2575324263477e4f6a15e3b56a315e0434266e092b2dd5a496d109cb15875256c73a2f0237c5332de28388693c643c8764f137e28e8220437f05b7659f58c4df94685";
-            var exponent = "10001";
-            rsa.setPublic(pbk, exponent);
-            var a = rsa.encrypt(oldPwd);
-            response.send(a);
-            console.log(a);
-        });
-    }
+            try 
+            {
+                var $ = window.$;
+                console.log("dom is done");
+                
+                window.KISSY.use("kg/rsa/2.0.1/index",function (S,e)
+                {
+                    var rsa = new e;
+                    var pbk = $("#J_PBK").val();
+                    // var pbk = "9a39c3fefeadf3d194850ef3a1d707dfa7bec0609a60bfcc7fe4ce2c615908b9599c8911e800aff684f804413324dc6d9f982f437e95ad60327d221a00a2575324263477e4f6a15e3b56a315e0434266e092b2dd5a496d109cb15875256c73a2f0237c5332de28388693c643c8764f137e28e8220437f05b7659f58c4df94685";
+                    var exponent = "10001";
+                    rsa.setPublic(pbk, exponent);
+                    var a = rsa.encrypt(oldPwd);
+                    response.send(a);
+                    console.log(a);
+                });
+            } catch (error) {
+                console.log(error)
+            }
+        }
     });
-    
 }
