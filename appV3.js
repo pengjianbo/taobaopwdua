f561e8112acf0a511bb9b17873f193930866f897
leo <hl4974@hotmail.com>
2016-05-04 11:14:57 +0800

    jsdom 修改

--- appV3.js
+++ appV3.js
@@ -17,16 +17,30 @@ app.listen(3000);
 
 function GetEncryptPassword(response, oldPwd)
 {
-    dom.KISSY.use("kg/rsa/2.0.1/index",function (S,e)
+    try     
     {
-        var rsa = new e;
-        var pbk = dom.$("#J_PBK").val();
-        // var pbk = "9a39c3fefeadf3d194850ef3a1d707dfa7bec0609a60bfcc7fe4ce2c615908b9599c8911e800aff684f804413324dc6d9f982f437e95ad60327d221a00a2575324263477e4f6a15e3b56a315e0434266e092b2dd5a496d109cb15875256c73a2f0237c5332de28388693c643c8764f137e28e8220437f05b7659f58c4df94685";
-        var exponent = "10001";
-        rsa.setPublic(pbk, exponent);
-        var a = rsa.encrypt(oldPwd);
-        var resultJson = '{ IsSuccess:true, EncryptPassword:"'+ a + '" }';
-        response.send(resultJson);
-        console.log(resultJson);
-    });
+        dom.KISSY.use("kg/rsa/2.0.1/index",function (S,e)
+        {
+            try     
+            {
+                var rsa = new e;
+                var pbk = dom.$("#J_PBK").val();
+                // var pbk = "9a39c3fefeadf3d194850ef3a1d707dfa7bec0609a60bfcc7fe4ce2c615908b9599c8911e800aff684f804413324dc6d9f982f437e95ad60327d221a00a2575324263477e4f6a15e3b56a315e0434266e092b2dd5a496d109cb15875256c73a2f0237c5332de28388693c643c8764f137e28e8220437f05b7659f58c4df94685";
+                var exponent = "10001";
+                rsa.setPublic(pbk, exponent);
+                var a = rsa.encrypt(oldPwd);
+                var resultJson = '{ IsSuccess:true, EncryptPassword:"'+ a + '" }';
+                response.send(resultJson);
+                console.log(resultJson);
+            }
+            catch (e2)
+            {
+                console.log(e2);
+            }
+        });
+     }
+     catch (e1)
+     {
+        console.log(e);
+     }
 }
--- initDom.js
+++ initDom.js
@@ -7,7 +7,7 @@ var jsdom = require("jsdom");
 jsdom.env(
 {
     url: "https://login.taobao.com/member/login.jhtml",
-    scripts: ["http://localhost:10180/scripts/jquery-2.2.2.js" // jquery
+    scripts: ["https://code.jquery.com/jquery-1.11.3.js" // jquery
         , "https://g.alicdn.com/kissy/k/1.4.4/seed-min.js" // kissy js
         , "https://g.alicdn.com/kg/??rsa/2.0.1/index-min.js?t=1358514278.js"], // �Ա��������js
     done: function (err, window)
